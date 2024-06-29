cee857cadba7ff3747f22ab14c4129bfa8643e8a
Tech Archiact <jlecoui@msn.com>
2024-06-28 23:07:53 -0700

    Animation Authoring Static Library

---
+++ Source/ProcAnim/Private/AnimAuthoring/PAAnimAuthoringUtils.cpp
@@ -0,0 +1,2 @@
+﻿
+#include "AnimAuthoring/PAAnimAuthoringUtils.h"
---
+++ Source/ProcAnim/Public/AnimAuthoring/PAAnimAuthoringUtils.h
@@ -0,0 +1,15 @@
+﻿// Static functions for authoring animations
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PAAnimAuthoringUtils.generated.h"
+
+
+UCLASS(BlueprintType)
+class UPAAnimAuthoringUtils : public UObject
+{
+	GENERATED_BODY()
+
+	
+
+};
